8e4cd12d089fe9c56965bb05e4e03feaf5bc00a3
jfarinaalvarez <jfarinaalvarez@atanasoff32.danielcastelao.org>
2015-10-19 09:07:32 +0200

    Javier fariñavhj

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -1,11 +1,17 @@
 package prueba;
 
+import java.util.Scanner;
+
 
 public class Prueba {
 
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Lado?");
+        float lado = sc.nextFloat(); 
+        float area = lado*lado;
+        System.out.println("El area es:\n"+ area);
     }
     
 }
